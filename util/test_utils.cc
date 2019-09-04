46d1b2b9268c29a603d7506174ef1b2d4cf41489
John Ousterhout <ouster@cs.stanford.edu>
2019-09-04 16:40:36 -0700

    Typo in test_utils.cc

--- util/test_utils.cc
+++ util/test_utils.cc
@@ -154,7 +154,7 @@ void seed_buffer(void *buffer, size_t length, int seed)
 }
 
 /**
- * printAddress() - Generate a human-readable description of an inet address.
+ * print_address() - Generate a human-readable description of an inet address.
  * @addr:    The address to print
  * @buffer:  Where to store the human readable description.
  * @size:    Number of bytes available in buffer.
