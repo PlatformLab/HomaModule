a8ee12ae9916f8dfd27bfc1336c61d330f8f9b81
Daniel Manjarres <danmanj@google.com>
2022-08-21 10:06:38 -0700

    Move IPPROTO_HOMA to 0xfd, which is reserved for experimentation.

--- homa_plumbing.c
+++ homa_plumbing.c
@@ -30,7 +30,7 @@ MODULE_VERSION("0.01");
 /* Homa's protocol number within the IP protocol space (this is not an
  * officially allocated slot).
  */
-#define IPPROTO_HOMA 140
+#define IPPROTO_HOMA 0xFD
 
 /* Not yet sure what these variables are for */
 long sysctl_homa_mem[3] __read_mostly;
