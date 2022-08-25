b5de7ff8bb53074fae3488032ea73bed2ef17ab4
Daniel Manjarres <danmanj@google.com>
2022-08-25 14:22:44 -0700

    cp_node.cc: fix memory leak.

--- util/cp_node.cc
+++ util/cp_node.cc
@@ -496,6 +496,7 @@ void init_server_addrs(void)
 		}
 		dest = reinterpret_cast<struct sockaddr_in *>
 				(matching_addresses->ai_addr);
+		freeaddrinfo(matching_addresses);
 		while (((int) first_id.size()) < node)
 			first_id.push_back(-1);
 		first_id.push_back((int) server_addrs.size());
