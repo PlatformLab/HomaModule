11b03e1fa0b3c1e3abbc1bb2abfac886268049f9
John Ousterhout <ouster@cs.stanford.edu>
2022-11-28 21:23:58 -0800

    Added homa::receiver class

---
+++ homa_receiver.cc
@@ -0,0 +1,120 @@
+/* Copyright (c) 2022 Stanford University
+ *
+ * Permission to use, copy, modify, and/or distribute this software for any
+ * purpose with or without fee is hereby granted, provided that the above
+ * copyright notice and this permission notice appear in all copies.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+ * WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+ * MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
+ * ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+ * WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
+ * ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
+ * OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+ */
+
+#include <string.h>
+
+#include "homa_receiver.h"
+
+/**
+ * homa::receiver::homa() - Constructor for receivers.
+ * @fd:         Homa socket from which this object will receive incoming
+ *              messages. The caller is responsible for setting up buffering
+ *              on the socket using setsockopt with the SO_HOMA_SET_BUF option.
+ *              The file descriptor must be valid for the lifetime of this
+ *              object.
+ * @buf_region: Location of the buffer region that was allocated for
+ *              this socket.
+ */
+homa::receiver::receiver(int fd, void *buf_region)
+	: fd(fd)
+	, hdr()
+	, control()
+	, source()
+        , msg_length(-1)
+        , buf_region(reinterpret_cast<char *>(buf_region))
+{
+	memset(&hdr, 0, sizeof(hdr));
+	hdr.msg_name = &source;
+	hdr.msg_namelen = sizeof(source);
+	hdr.msg_control = &control;
+	hdr.msg_controllen = sizeof(control);
+
+	memset(&control, 0, sizeof(control));
+}
+
+/**
+ * homa::receiver::~homa() - Destructor for homa::receivers. The main purpose of
+ * this destructor is to return any residual buffers to Homa.
+ */
+homa::receiver::~receiver()
+{
+	release();
+}
+
+/**
+ * homa::receiver::copy_out() - Copy data out of the current message.
+ * @dest:     Data will be copied here.
+ * @offset:   Offset within the message of the first byte to copy.
+ * @count:    Number of bytes to copy; if the message doesn't contain
+ *            this many bytes starting at offset, then only the
+ *            available number of bytes will be copied.
+ */
+void homa::receiver::copy_out(void *dest, size_t offset, size_t count) const
+{
+	ssize_t limit = offset + count;
+	char *cdest = static_cast<char *>(dest);
+
+	if (limit > msg_length)
+		limit = msg_length;
+	while (static_cast<ssize_t>(offset) < limit) {
+		size_t chunk_size = contiguous(offset);
+		memcpy(cdest, get<char>(offset), chunk_size);
+		offset += chunk_size;
+		cdest += chunk_size;
+	}
+}
+
+/**
+ * homa::receiver::receive() - Receive an incoming message and release
+ * resources for the current message, if any.
+ * @flags:    Various OR'ed bits such as HOMA_RECVMSG_REQUEST and
+ *            HOMA_RECVMSG_NONBLOCKING. See the Homa documentation
+ *            for the flags field of recvmsg for details.
+ * @id:       Identifier of a particular RPC whose result is desired,
+ *            or 0. See the Homa documentation for the id field of
+ *            recvmsg for details.
+ * Return:    The length of the new active message. If an error occurs, -1
+ *            is returned and additional information is available in
+ *            errno. Note: if id() returns a nonzero result after an
+ *            error, it means that RPC has now completed with an error
+ *            and errno describes the nature of the error.
+ */
+size_t homa::receiver::receive(int flags, uint64_t id)
+{
+	control.flags = flags;
+	control.id = id;
+	msg_length = recvmsg(fd, &hdr, 0);
+	if (msg_length < 0) {
+		control.num_buffers = 0;
+		id = 0;
+	}
+	return msg_length;
+}
+
+/**
+ * homa::receiver::release() - Release any resources associated with the
+ * current message, if any.
+ */
+void homa::receiver::release()
+{
+	if (control.num_buffers == 0)
+		return;
+
+	/* This recvmsg request will do nothing except return buffer space. */
+	control.flags = HOMA_RECVMSG_NONBLOCKING;
+	control.id = 0;
+	recvmsg(fd, &hdr, 0);
+	control.num_buffers = 0;
+}
---
+++ homa_receiver.h
@@ -0,0 +1,167 @@
+/* Copyright (c) 2022 Stanford University
+ *
+ * Permission to use, copy, modify, and/or distribute this software for any
+ * purpose with or without fee is hereby granted, provided that the above
+ * copyright notice and this permission notice appear in all copies.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+ * WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+ * MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
+ * ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+ * WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
+ * ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
+ * OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+ */
+
+#pragma once
+
+#include <sys/socket.h>
+#include <sys/types.h>
+
+#include "homa.h"
+
+namespace homa {
+
+/**
+ * class homa::receiver - Helper class for receiving a series of messages
+ * from a Homa socket. At any given time there may be a single incoming
+ * message associated with the object (the "current" message); receiving the
+ * next message releases resources for any existing current message.
+ *
+ * This class serves two purposes: first, it implements the application side
+ * of the Homa buffer management protocol, returning receive buffer space to
+ * Homa when the application longer needs it. Second, it provides convenience
+ * methods for accessing messages that are scattered over several discontiguous
+ * regions of buffer space.
+ */
+class receiver {
+public:
+	receiver(int fd, void *buf_regio);
+	~receiver();
+
+	/**
+	 * homa::receiver::contiguous() - Return a count of the number
+	 * of contiguous bytes that are available in the current message
+	 * at a given offset. Zero is returned if there is no current message
+	 * or the offset is beyond the end of the message.
+	 * @offset:  An offset from the beginning of the current message.
+	 */
+	inline size_t contiguous(size_t offset) const
+	{
+		size_t bpage = offset >> HOMA_BPAGE_SHIFT;
+		size_t bpage_end = (bpage == (control.num_buffers-1))
+				? (msg_length & (HOMA_BPAGE_SIZE-1))
+				: HOMA_BPAGE_SIZE;
+		size_t result = (static_cast<ssize_t>(offset) >= msg_length) ? 0
+				: bpage_end - (offset & (HOMA_BPAGE_SIZE-1));
+		return result;
+	}
+
+	/**
+	 * homa::receiver::completion_cookie() - Return the completion
+	 * cookie associated with the current message; result is undefined
+	 * if there is no current message.
+	 */
+	uint64_t completion_cookie() const
+	{
+		return control.completion_cookie;
+	}
+
+	void copy_out(void *dest, size_t offset, size_t count) const;
+
+	/**
+	 * homa::receiver::get() - Make part of the current message
+	 * accessible.
+	 * @offset:   Offset within the message of the first byte of an object
+	 *            of type T
+	 * @storage:  Pointer to a memory region containing at least sizeof(T)
+	 *            bytes. If the desired object's bytes are not currently in
+	 *            contiguous storage in the message, and if this argument
+	 *            is non-null, information is copied out of the message
+	 *            into this object so that it is contiguous.
+	 * Return:    A pointer to the desired object (either in the message
+	 *            or at *storage), or nullptr if the object could not be
+	 *            returned (because it extended beyond the end of the
+	 *            message, or it wasn't contiguous and storage was nullptr)
+	 */
+	template<typename T>
+	inline T* get(size_t offset, T* storage = nullptr) const {
+		int buf_num = offset >> HOMA_BPAGE_SHIFT;
+		if (static_cast<ssize_t>(offset + sizeof(T)) > msg_length)
+			return nullptr;
+		if (contiguous(offset))
+			return reinterpret_cast<T*>(buf_region
+					+ control.buffers[buf_num]
+					+ (offset & (HOMA_BPAGE_SIZE - 1)));
+		if (storage)
+			copy_out(storage, offset, sizeof(T));
+		return storage;
+	}
+
+	/**
+	 * id() - Return the Homa RPC identifier for the current message,
+	 * or 0 if there is no current message.
+	 */
+	inline uint64_t id() const
+	{
+		return control.id;
+	}
+
+	/**
+	 * homa::receiver::is_request() - Return true if the current message
+	 * is a request, and false if it is a response or if there is no
+	 * current message.
+	 */
+	bool is_request() const
+	{
+		return control.id & 1;
+	}
+
+	/**
+	 * homa::receiver::length() - Return the total number of bytes
+	 * current message, or a negative value if there is no current
+	 * message.
+	 */
+	ssize_t length() const
+	{
+		return msg_length;
+	}
+
+	size_t receive(int flags, uint64_t id);
+	void release();
+
+	/**
+	 * homa::receiver::src_addr() - Return a pointer to the address
+	 * of the sender of the current message. The result is undefined
+	 * if there is no current message.
+	 */
+	const sockaddr_in_union *src_addr() const
+	{
+		return &source;
+	}
+
+protected:
+	/** @fd: File descriptor for an open Homa socket. */
+	int fd;
+
+	/** @hdr: Used to pass information to the recvmsg system call. */
+	struct msghdr hdr;
+
+	/**
+	 * @control: Additional Homa-specific information passed to the
+	 * recvmsg system call through hdr->msg_control. Note: if
+	 * num_buffers != 0, it means this contains buffers from a previous
+	 * message that must be returned to Homa.
+	 */
+	struct homa_recvmsg_control control;
+
+	/** @source: Address of the node that sent the current message. */
+	sockaddr_in_union source;
+
+	/** @length: Length of the current message, or < 0  if none. */
+	ssize_t msg_length;
+
+	/** @buf_region: First byte of buffer space for this message. */
+	char *buf_region;
+};
+}    // namespace homa
