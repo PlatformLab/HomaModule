b1601589c7ab8ee28b7216e5fc7c5f9ab91bcea0
John Ousterhout <ouster@cs.stanford.edu>
2024-04-09 16:05:02 -0700

    Remove stale "__attribute__((unused))" declarations

--- test/kselftest_harness.h
+++ test/kselftest_harness.h
@@ -822,9 +822,7 @@ void __run_test(struct __test_metadata *t)
  * @verbose: Nonzero means print all test names as they run; zero means print
  *           only for test failures.
  */
-static int test_harness_run(int __attribute__((unused)) argc,
-			    char __attribute__((unused)) **argv,
-			    int verbose)
+static int test_harness_run(int  argc, char  **argv, int verbose)
 {
 	struct __test_metadata *t;
 	int ret = 0;
