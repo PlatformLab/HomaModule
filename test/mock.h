1e251b38585d3d4a4f79041d0bf4e56b7c87e3cd
John Ousterhout <ouster@cs.stanford.edu>
2025-01-09 10:12:30 -0800

    Move mocking definitions from homa_impl.h to mock.h

--- test/mock.h
+++ test/mock.h
@@ -1,9 +1,74 @@
 /* Copyright (c) 2019-2022 Homa Developers
  * SPDX-License-Identifier: BSD-1-Clause
  */
+#ifndef _HOMA_MOCK_H
+#define _HOMA_MOCK_H
+
+/* Replace various Linux variables and functions with mocked ones. */
+#undef alloc_pages
+#define alloc_pages mock_alloc_pages
+
+#define compound_order mock_compound_order
+
+#ifdef cpu_to_node
+#undef cpu_to_node
+#endif
+#define cpu_to_node mock_cpu_to_node
+
+#undef current
+#define current current_task
+
+#undef DECLARE_PER_CPU
+#define DECLARE_PER_CPU(type, name) extern type name[10]
+
+#undef DEFINE_PER_CPU
+#define DEFINE_PER_CPU(type, name) type name[10]
+
+#define get_page mock_get_page
+
+#undef kmalloc
+#define kmalloc mock_kmalloc
+
+#undef kmalloc_array
+#define kmalloc_array(count, size, type) mock_kmalloc((count) * (size), type)
+
+#define kthread_complete_and_exit(...)
+
+#ifdef page_address
+#undef page_address
+#endif
+#define page_address(page) ((void *)page)
+
+#define page_ref_count mock_page_refs
+
+#define page_to_nid mock_page_to_nid
+
+#undef per_cpu
+#define per_cpu(name, core) (name[core])
+
+#define put_page mock_put_page
+
+#define rcu_read_lock mock_rcu_read_lock
+
+#define rcu_read_unlock mock_rcu_read_unlock
+
+#undef register_net_sysctl
+#define register_net_sysctl mock_register_net_sysctl
+
+#define signal_pending(...) mock_signal_pending
+
+#define spin_unlock mock_spin_unlock
+
+#undef vmalloc
+#define vmalloc mock_vmalloc
+
+/* Forward references: */
+struct homa;
+struct homa_rpc;
+struct homa_sock;
+struct homa_socktab;
 
 /* Functions for mocking that are exported to test code. */
-
 extern int         mock_alloc_page_errors;
 extern int         mock_alloc_skb_errors;
 extern int         mock_bpage_size;
@@ -36,6 +101,7 @@ extern int         mock_numa_mask;
 extern int         mock_page_nid_mask;
 extern char        mock_printk_output[];
 extern int         mock_route_errors;
+extern int         mock_signal_pending;
 extern int         mock_spin_lock_held;
 extern struct task_struct
 		   mock_task;
@@ -44,17 +110,23 @@ extern int         mock_vmalloc_errors;
 extern int         mock_xmit_log_verbose;
 extern int         mock_xmit_log_homa_info;
 
+extern struct task_struct *current_task;
+
 struct page *
-		   mock_alloc_pages(gfp_t gfp, unsigned order);
+	    mock_alloc_pages(gfp_t gfp, unsigned order);
 int         mock_check_error(int *errorMask);
 void        mock_clear_xmit_prios(void);
+unsigned int mock_compound_order(struct page *page);
+int         mock_cpu_to_node(int core);
 void        mock_data_ready(struct sock *sk);
 cycles_t    mock_get_cycles(void);
 unsigned int
 	    mock_get_mtu(const struct dst_entry *dst);
 void        mock_get_page(struct page *page);
+void       *mock_kmalloc(size_t size, gfp_t flags);
 int         mock_page_refs(struct page *page);
 int         mock_page_refs(struct page *page);
+int         mock_page_to_nid(struct page *page);
 void        mock_put_page(struct page *page);
 void        mock_rcu_read_lock(void);
 void        mock_rcu_read_unlock(void);
@@ -69,10 +141,12 @@ void        mock_spin_unlock(spinlock_t *lock);
 int         mock_skb_count(void);
 struct sk_buff *
             mock_skb_new(struct in6_addr *saddr, struct homa_common_hdr *h,
-			int extra_bytes, int first_value);
+			 int extra_bytes, int first_value);
 void        mock_sock_destroy(struct homa_sock *hsk,
 			      struct homa_socktab *socktab);
 void        mock_sock_init(struct homa_sock *hsk, struct homa *homa,
 			   int port);
 void        mock_teardown(void);
 void       *mock_vmalloc(size_t size);
+
+#endif /* _HOMA_MOCK_H */
